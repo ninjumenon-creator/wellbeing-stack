62925efc759f432629d049fa1a4fc720efc4c2d4
Niranjana M <ninjumenon@gmail.com>
2026-03-05 14:54:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
 let posts = JSON.parse(localStorage.getItem("posts")) || [];
 
 function renderPosts(){
-let feed = document.getElementById("feed");
-feed.innerHTML = "";
 
-posts.forEach(post => {
-let div = document.createElement("div");
-div.className = "post";
-div.innerText = post;
+let feed = document.getElementById("feed");
+feed.innerHTML="";
+
+posts.forEach(p=>{
+
+let div=document.createElement("div");
+div.className="post";
+div.innerText=p;
+
 feed.appendChild(div);
+
 });
+
 }
 
 function addPost(){
 
-let input = document.getElementById("postInput");
+let input=document.getElementById("postInput");
 
-if(input.value.trim() === "") return;
+if(input.value.trim()==="") return;
 
 posts.unshift(input.value);
 
-localStorage.setItem("posts", JSON.stringify(posts));
+localStorage.setItem("posts",JSON.stringify(posts));
 
-input.value = "";
+input.value="";
 
 renderPosts();
+
+}
+
+function clearPosts(){
+
+posts=[];
+
+localStorage.removeItem("posts");
+
+renderPosts();
+
 }
 
 renderPosts();
